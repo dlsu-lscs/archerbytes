75c2c053628a6001afe492809318e5220aa43c66
Alec Nono <alecbnono@gmail.com>
2025-10-29 21:41:34 +0800

    refactor(article): place article content separately

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,8 +1,8 @@
 import Sidebar from '@/components/organisms/Sidebar';
 import RelatedSidebar from '@/features/article/components/organisms/RelatedSidebar';
 import ArticleHeader from '@/features/article/components/organisms/ArticleHeader';
+import ArticleContent from '@/features/article/components/molecules/ArticleContent';
 import Discussion from '@/features/article/components/organisms/Discussion';
-import Photo from '@/features/article/components/atoms/Photo';
 
 export default function ArticlePage() {
     return (
@@ -13,48 +13,7 @@ export default function ArticlePage() {
             <main className="flex flex-col gap-[30px] px-10">
                 <section className="flex flex-col gap-[10px]">
                     <ArticleHeader />
-                    <Photo source="/image.jpg" />
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-                        exercitationem reiciendis ducimus eligendi veritatis eaque dolorum
-                        sed maiores voluptatem soluta dolores placeat nam tenetur
-                        accusantium dicta, dolorem perspiciatis voluptatum hic. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Deserunt voluptatem
-                        doloremque autem velit itaque atque perspiciatis, excepturi,
-                        exercitationem vitae asperiores minus libero nostrum deleniti alias
-                        dolore non numquam eligendi reprehenderit? Lorem ipsum dolor sit
-                        amet consectetur adipisicing elit. Totam sint, facilis alias aliquid
-                        ab repudiandae facere vel numquam deserunt. Architecto impedit vel
-                        consequuntur libero officiis harum eos in error dicta?
-                    </p>
-                    <Photo source="/image.jpg" />
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-                        exercitationem reiciendis ducimus eligendi veritatis eaque dolorum
-                        sed maiores voluptatem soluta dolores placeat nam tenetur
-                        accusantium dicta, dolorem perspiciatis voluptatum hic. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Deserunt voluptatem
-                        doloremque autem velit itaque atque perspiciatis, excepturi,
-                        exercitationem vitae asperiores minus libero nostrum deleniti alias
-                        dolore non numquam eligendi reprehenderit? Lorem ipsum dolor sit
-                        amet consectetur adipisicing elit. Totam sint, facilis alias aliquid
-                        ab repudiandae facere vel numquam deserunt. Architecto impedit vel
-                        consequuntur libero officiis harum eos in error dicta?
-                    </p>
-                    <Photo source="/image.jpg" />
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-                        exercitationem reiciendis ducimus eligendi veritatis eaque dolorum
-                        sed maiores voluptatem soluta dolores placeat nam tenetur
-                        accusantium dicta, dolorem perspiciatis voluptatum hic. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Deserunt voluptatem
-                        doloremque autem velit itaque atque perspiciatis, excepturi,
-                        exercitationem vitae asperiores minus libero nostrum deleniti alias
-                        dolore non numquam eligendi reprehenderit? Lorem ipsum dolor sit
-                        amet consectetur adipisicing elit. Totam sint, facilis alias aliquid
-                        ab repudiandae facere vel numquam deserunt. Architecto impedit vel
-                        consequuntur libero officiis harum eos in error dicta?
-                    </p>
+                    <ArticleContent />
                 </section>
                 <Discussion />
             </main>
