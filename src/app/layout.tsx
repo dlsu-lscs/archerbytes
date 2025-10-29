05a379bda01d79e1dd9a318c8c439835a8642c4d
Rainer Gonzaga <gonzagarainer30@gmail.com>
2025-10-29 19:18:40 +0800

    chore: fix duplicate import problem due to merge conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import Navbar from '@/components/organisms/Navbar';
 import Footer from '@/components/organisms/Footer';
 
