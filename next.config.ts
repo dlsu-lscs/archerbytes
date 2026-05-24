e1c19c794412ed21480af64c28fdc6d76439cc40
Rainer Gonzaga <rainerdgonzaga@gmail.com>
2026-05-25 02:37:13 +0800

    fix(config): add local patterns to next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
+        localPatterns: [
+            {
+                pathname: '/**',
+            },
+            {
+                pathname: '/api/storage/profile-image',
+            },
+        ],
         remotePatterns: [
             new URL('https://lh3.googleusercontent.com/a/**'),
             new URL('https://cms.app.dlsu-lscs.org/**'),
