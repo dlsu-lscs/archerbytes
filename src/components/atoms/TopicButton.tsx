7eff3791955fd01b5a7147574e6d8e6f5dbab567
420Rain <dulatrerain@gmail.com>
2026-05-25 14:10:43 +0800

    feat: topics/categories in the sidebar now uses real data

--- src/components/atoms/TopicButton.tsx
+++ src/components/atoms/TopicButton.tsx
@@ -3,7 +3,7 @@ import { TopicType } from '../../types/topic.types';
 
 export default function TopicButton({ children }: TopicType) {
   return (
-    <Button className="rounded-full bg-neutral-400 text-neutral-900 h-7 border-solid border-2 border-neutral-950 py-4">
+    <Button className="rounded-full bg-neutral-400 text-neutral-900 h-7 border-solid border-2 border-neutral-950 py-4 w-full">
       {children}
     </Button>
   );
--- src/components/organisms/RelatedSidebar.tsx
+++ src/components/organisms/RelatedSidebar.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import TopicSearchSection from '../molecules/TopicSearchSection';
 import SavedArticlesSection from '../molecules/SavedArticlesSection';
+import useCategoryList from '@/features/article/queries/useCategoryList';
 
 export default function RelatedSidebar() {
+  const {data: categories, isLoading, isError} = useCategoryList();
+
   return (
     <div className="hidden lg:flex flex-col gap-6 p-5 max-w-64">
-      <TopicSearchSection />
+      <TopicSearchSection categories={categories} isLoading={isLoading} isError={isError} />
       <SavedArticlesSection />
     </div>
   );
